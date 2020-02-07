ec128f0652d531a7db818ddbc0cad4f3cf0bd2cb
Julian <julian.kimmig@gmx.net>
2020-02-07 07:47:00 +0100

    updated cmd setting

--- json_websocket/cmd/abstract_cmd_json_socket.py
+++ json_websocket/cmd/abstract_cmd_json_socket.py
@@ -1,6 +1,3 @@
-import json
-from typing import Dict, Any
-
 from ..basic.abstract_json_socket import MessageType, AbstractJsonWebsocket
 
 
@@ -18,12 +15,12 @@ def merge(new_values, default_values):
     return nd
 
 
-def run_cmd(consumer, source, cmd, data):
-    f = consumer.available_cmds[cmd]
+def run_cmd(consumer, cmd, data):
+    f = consumer.get_cmd(cmd)
+    if f is None:
+        raise AttributeError(str(cmd) + " not found")
     if f.accept_consumer:
         data["consumer"] = consumer
-    if f.accept_source:
-        data["source"] = source
     return f(**data)
 
 
@@ -37,19 +34,23 @@ MESSAGETYPES = {
 class AbstractCmdJsonWebsocket(AbstractJsonWebsocket):
     def __init__(self):
         AbstractJsonWebsocket.__init__(self)
-        self.available_cmds = {}
+        self._available_cmds = {}
 
         for n, t in MESSAGETYPES.items():
             self.set_message_type(n, t)
+
+    def get_cmd(self, cmd):
+        return self._available_cmds.get(cmd)
 
     def set_cmd(self, cmd, func):
         varnames = func.__code__.co_varnames
         def call_func(*args, **kwargs):
             return func(*args, **kwargs)
 
+        print(cmd, varnames)
         setattr(call_func, "accept_consumer", "consumer" in varnames)
         setattr(call_func, "accept_source", "source" in varnames)
-        self.available_cmds[cmd] = call_func
+        self._available_cmds[cmd] = call_func
 
     def cmd_message(self, cmd, **data):
         return self.message_types["cmd"].encode(cmd=cmd, data=data)
